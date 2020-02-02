228c9d9c5f2d950c6d4dd63ebfe3bb82ab8a82db
gscaparrotti <giacomo.scaparrotti@studio.unibo.it>
2020-02-02 16:12:00 +0100

    improved time logging

--- model/defaultCallbacks.cc
+++ model/defaultCallbacks.cc
@@ -1,5 +1,8 @@
 #include "defaultCallbacks.h"
 #include "ns3/core-module.h"
+#include <iostream>
+#include <iomanip>
+
 
 using namespace ns3;
 
@@ -8,17 +11,21 @@ NS_LOG_COMPONENT_DEFINE("ns3asy-cbs");
 void ConnectionAccepted(const char receiverIp[], unsigned int receiverPort, const char senderIp[],
 		unsigned int senderPort, double time) {
 	NS_LOG_DEBUG("A connection has been accepted by the socket " << receiverIp << ":"
-			<< receiverPort << "; it was requested by " << senderIp << ":" << senderPort << " @ " << time);
+			<< receiverPort << "; it was requested by " << senderIp << ":" << senderPort << " @ "
+			<< std::setprecision(10) << std::fixed << time);
 }
 
 void PacketReceived(const char ip[], unsigned int port, double time) {
-	NS_LOG_DEBUG("A packet has been received by the socket with ip=" << ip << " and port=" << port << " @ " << time);
+	NS_LOG_DEBUG("A packet has been received by the socket with ip=" << ip << " and port=" << port << " @ "
+			<< std::setprecision(10) << std::fixed << time);
 }
 
 void PacketRead(const char receiverIp[], unsigned int receiverPort, const char senderIp[],
 		unsigned int senderPort, const unsigned char payload[], unsigned int payloadLength, double time) {
 	std::ostringstream outputDebug;
 	std::ostringstream outputInfo;
+	outputDebug << std::setprecision(10);
+	outputDebug << std::fixed;
 	outputDebug << "Some packets has been read by the socket " << receiverIp << ":"
 			<< receiverPort << "; it was sent by " << senderIp << ":" << senderPort << " @ " << time;
 	outputInfo << "The read content is: ";
@@ -33,6 +40,8 @@ void PacketSent(const char senderIp[], unsigned int senderPort, const char recei
 		unsigned int receiverPort, const unsigned char payload[], unsigned int payloadLength, double time) {
 	std::ostringstream outputDebug;
 	std::ostringstream outputInfo;
+	outputDebug << std::setprecision(10);
+	outputDebug << std::fixed;
 	outputDebug << "A packet has been sent by the socket " << senderIp << ":"
 			<< senderPort << "; it was sent to " << receiverIp << ":" << receiverPort << " @ " << time;
 	outputInfo << "The sent content is: ";
